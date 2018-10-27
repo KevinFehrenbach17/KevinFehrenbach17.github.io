535667ff0e2bf10008e7810826d1120e094b2580
Kevin Fehrenbach <kevinfehrenbach17@gmail.com>
2018-10-27 14:34:55 -0400

    Added JavaScript and Changed the CSS a bit, looks better!

---
+++ app.js
@@ -0,0 +1,38 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+// collapsable desc.
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+// alert box
+
+var alertButton = document.getElementById('alert');
+alertButton.onclick = function() {
+    alert('Are you sure you want to leave?')
+}
